cac0610b120923ba6bff178e44a388f27d44c7bd
Richard Kovacs <richardkovacs.it@gmail.com>
2023-08-22 21:32:57 +0200

    feat(jwt): incorporate api token into jwt

--- wp-content/plugins/audioblog/audioblog-jwt-endpoint.php
+++ wp-content/plugins/audioblog/audioblog-jwt-endpoint.php
@@ -24,13 +24,14 @@ add_action( 'rest_api_init', function () {
 // Function to generate JWT
 function audioblog_jwt_generate_token() {
     $key = get_option('audioblog_jwt_secret_key');  // Retrieve the secret key from the settings
+    $api_token = get_option('audioblog_api_token');  // Retrieve the API token from the settings
 
     // Sample payload. You can adjust this as per your needs
     $payload = array(
         "iss" => get_site_url(),
         "iat" => time(),
         "exp" => time() + (60 * 60),  // Token valid for 1 hour
-        "data" => "sample data"  // Placeholder data
+        "api_token" => $api_token  // Placeholder data
     );
 
     // Encode the payload into JWT
